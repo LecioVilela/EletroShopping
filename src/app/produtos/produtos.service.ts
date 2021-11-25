81092bdc6e84355b3a6cd8b8c182105ebaaebdf6
LecioVilela <lecio.vilela@yahoo.com>
2021-11-25 10:42:42 -0300

    update project

--- src/app/produtos/produtos.service.ts
+++ src/app/produtos/produtos.service.ts
@@ -1,5 +1,7 @@
+import { Produto } from './produto';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 @Injectable()
 export class ProdutoService {
 
@@ -7,7 +9,8 @@ export class ProdutoService {
     
     protected UrlServiceV1: string = "http://localhost:3000/";
 
-    getProducts() {
-
+    getProducts() : Observable<Produto[]> {
+        return this.http
+        .get<Produto[]>(this.UrlServiceV1 + "produtos");
     }
 }
